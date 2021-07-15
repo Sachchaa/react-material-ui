96dfce867ab0e3c61933976a8677e800fda72667
Sachin Kanishka <sachinkanishka001@gmail.com>
2021-07-16 00:22:01 +0930

    #day02 -
    covered checkbox

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css';
-import Button from '@material-ui/core/Button'
-import ButtonGroup from '@material-ui/core/ButtonGroup'
 import SaveIcon from '@material-ui/icons/Save'
 import DeleteIcon from '@material-ui/icons/Delete'
+import { Checkbox, Button, ButtonGroup } from '@material-ui/core'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+
+function CheckboxExample() {
+  const [checked, setChecked] = useState(true)
+  return (
+    <FormControlLabel
+      control={
+        <Checkbox
+          checked={checked}
+          onChange={(e) => setChecked(e.target.checked)}
+        //icon and checkedIcon use to change checkbox icon
+        />
+      }
+      label="Testing Checkbox"
+    />
+
+  )
+}
 
 function App() {
   return (
     <div className="App">
+      <CheckboxExample />
       <ButtonGroup variant="contained">
-        <Button color="primary" startIcon={<SaveIcon />}>
+        <Button
+          color="primary"
+          startIcon={<SaveIcon />}
+        >
           Save
-      </Button>
-        <Button color="secondary" startIcon={<DeleteIcon />}>
+        </Button>
+        <Button
+          color="secondary"
+          startIcon={<DeleteIcon />}
+        >
           Discard
-      </Button>
+        </Button>
       </ButtonGroup>
 
 
     </div>
-  );
+  )
 }
 
 export default App;
