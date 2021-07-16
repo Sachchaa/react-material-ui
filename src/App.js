be83801a1ada6e5873e5543ccd8130895af3209d
Sachin Kanishka <sachinkanishka001@gmail.com>
2021-07-16 23:12:48 +0930

    #day02 -
    covered textfield
    covered makestyles

--- src/App.js
+++ src/App.js
@@ -4,6 +4,24 @@ import SaveIcon from '@material-ui/icons/Save'
 import DeleteIcon from '@material-ui/icons/Delete'
 import { Checkbox, Button, ButtonGroup } from '@material-ui/core'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
+import TextField from '@material-ui/core/TextField'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg, #FE688B, #FFBE53)',
+    border: 0,
+    marginBottom: 15,
+    borderRadius: 15,
+    color: 'white',
+    padding: '5px 30px'
+  }
+})
+
+function ButtonStyled() {
+  const classes = useStyles()
+  return <Button className={classes.root}>Test Styled Button</Button>
+}
 
 function CheckboxExample() {
   const [checked, setChecked] = useState(true)
@@ -25,23 +43,31 @@ function CheckboxExample() {
 function App() {
   return (
     <div className="App">
-      <CheckboxExample />
-      <ButtonGroup variant="contained">
-        <Button
-          color="primary"
-          startIcon={<SaveIcon />}
-        >
-          Save
-        </Button>
-        <Button
+      <header className="App-header">
+        <ButtonStyled />
+        <TextField
+          variant="outlined"
           color="secondary"
-          startIcon={<DeleteIcon />}
-        >
-          Discard
+          type="email"
+          label="Email"
+          placeholder="test@test.com"
+        />
+        <CheckboxExample />
+        <ButtonGroup variant="contained">
+          <Button
+            color="primary"
+            startIcon={<SaveIcon />}
+          >
+            Save
         </Button>
-      </ButtonGroup>
-
-
+          <Button
+            color="secondary"
+            startIcon={<DeleteIcon />}
+          >
+            Discard
+        </Button>
+        </ButtonGroup>
+      </header>
     </div>
   )
 }
